e6e66c778a294f36f795a5b8daad78430118f7fd
asaf <asafsj1@gmail.com>
2023-01-31 17:27:46 -0600

    second commit

---
+++ week2/Day2/DailyChallenge/script.js
@@ -0,0 +1,45 @@
+// Exercise 1:
+
+// Using this array :
+
+// const fruits = ["Banana", "Apples", "Oranges", "Blueberries"];
+// 1. Remove Banana from the array.
+// 2. Sort the array in alphabetical order.
+// 3. Add “Kiwi” to the end of the array.
+// 4. Remove “Apples” from the array. Don’t use the same method as in part 1.
+// 5. Sort the array in reverse order. (Not alphabetical, but reverse the current Array i.e. [‘a’, ‘c’, ‘b’] becomes [‘b’, ‘c’, ‘a’])
+// At the end you should see this outcome:
+
+// ["Kiwi", "Oranges", "Blueberries"]
+
+const fruits = ["Banana", "Apples", "Oranges", "Blueberries"];
+fruits.splice(0,1);
+console.log(fruits)
+
+
+fruits.sort()
+console.log(fruits)
+
+
+fruits.push('kiwi')
+console.log(fruits) 
+
+
+delete fruits [0]
+console.log(fruits)
+
+fruits.reverse()
+fruits.pop()  //borra el ultimo elemento
+console.log(fruits)
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ week2/Day2/ExercisesXPGold/script.js
@@ -0,0 +1,51 @@
+// excersice 1
+
+
+
+
+
+// EXERCISE 2
+
+let sentence = ["my","favorite","color","is","blue"];
+console.log(sentence);
+
+// 
+
+
+// 1.Create 2 variables. The values should be strings. For example:
+
+let str1 = "mix";
+let str2 = "pod";
+
+
+// 2. Slice out and swap the first 2 characters of the two strings from part 1.
+let changeLet = str1.slice(0,2) + str2.slice(2,3);
+str1 = str2.slice(0,2) + str1.slice(2,3);
+str2 = changeLet;
+
+
+// 3. Create a third variable where the value is the concatenation of the two strings from the part 1 (separated by a space).
+let newString = (`${str1} ${str2}`)
+
+// 4. Finally console.log the new concatenated string.
+console.log(newString)
+
+
+// Exercise 3 : Calculator
+// Instructions
+// Make a Calculator. Follow the instructions:
+
+// 1. Prompt the user for the first number.
+
+// 2. Store the first number in a variable called num1.
+
+// Hint : console.log the type of the variable num1. What should you do to convert it to a number ?
+
+// 3. Prompt the user for the second number.
+
+// 4. Store the second number in a variable called num2.
+
+// 5. Create an Alert where the value is the SUM of num1 and num2.
+
+// 6. BONUS: Make a program that can subtract, multiply, and also divide!
+
