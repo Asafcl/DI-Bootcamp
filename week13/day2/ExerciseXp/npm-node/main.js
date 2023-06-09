17c4ec4439b51f891818a7f16a43bc37e16b1566
asaf <asafsj1@gmail.com>
2023-06-08 19:18:34 -0600

    first commit

---
+++ week13/day2/ExerciseXp/npm-node/main.js
@@ -0,0 +1,23 @@
+// exercise 1
+
+// const a = 5;
+// const b = 10;
+
+
+//     console.log('hello you are ' + (a+b))
+
+// exercise 2
+
+console.log('hello how are you')
+
+// exercise 3
+
+const a = 5;
+const b = 10;
+let x;
+const c = 6;
+x = a + b;
+
+
+console.log('a+b= ',a+b)
+console.log('x + c= ',x + c)
